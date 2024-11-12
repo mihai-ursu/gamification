1fb0b63b54a4828d1a80869474ecfeec0ba750c9
Mihai <ursu.mihai.mail@gmail.com>
2024-11-12 21:49:08 +0200

    Footer WIP

--- src/components/course/footer/footer.tsx
+++ src/components/course/footer/footer.tsx
@@ -1,7 +1,19 @@
+"use client";
+
+import useQuestionsStore from "@/store/course";
+import { Button } from "../questions/answer-button";
+
 const QuestionsFooter = () => {
+  const { correctQuestions, selectedAnswer, setSelectedAnswer } =
+    useQuestionsStore();
+
   return (
-    <footer className="bg-slate-200 px-8 py-14">
-      <div className="ml-auto mr-auto max-w-[1460px]">QuestionsFooter</div>
+    <footer className="bg-slate-100 px-8 py-14">
+      <div className="ml-auto mr-auto flex max-w-[1460px] justify-end">
+        <Button variant="green" disabled={!selectedAnswer}>
+          Check Answer
+        </Button>
+      </div>
     </footer>
   );
 };
